a09f72cdfbc43016e4072fb19e3ef9e93600d891
Vinoth Michael Xavier <vinoth.michaelxavier@gmail.com>
2020-03-13 11:43:22 -0400

    move application route to services route:

--- tests/unit/routes/services-test.js
+++ tests/unit/routes/services-test.js
@@ -2,12 +2,12 @@ import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 import { testData } from 'suitepad/tests/fixtures/data';
 
-module('Unit | Route | application', function(hooks) {
+module('Unit | Route | services', function(hooks) {
   setupTest(hooks);
 
   test('model calls retrieval methods in sequence', async function(assert) {
     assert.expect(3);
-    const route = this.owner.lookup('route:application');
+    const route = this.owner.lookup('route:services');
     route._getData = () => { assert.step('_getData'); };
     route._getJSON = () => { assert.step('_getJSON'); };
     await route.model();
@@ -16,7 +16,7 @@ module('Unit | Route | application', function(hooks) {
 
   test('_getData calls fetch with given url', function(assert) {
     assert.expect(1);
-    const route = this.owner.lookup('route:application');
+    const route = this.owner.lookup('route:services');
     const url = 'https://gist.githubusercontent.com/test.json';
     window.fetch = fetchURL => {
       assert.equal(fetchURL, url, `expect fetch from : ${url}`);
@@ -26,7 +26,7 @@ module('Unit | Route | application', function(hooks) {
 
   test('_getJSON return data in json format', async function(assert) {
     assert.expect(1);
-    const route = this.owner.lookup('route:application');
+    const route = this.owner.lookup('route:services');
     window.fetch = () => { return { json: () =>  testData } }
     const result = await route._getJSON(fetch());
     assert.deepEqual(testData, result, "return expected data");
