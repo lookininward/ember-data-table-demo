d1487a25da74f163c29a0fcd17e6a4e5928d010c
Vinoth Michael Xavier <vinoth.michaelxavier@gmail.com>
2020-03-14 14:45:05 -0400

    configure env variables

--- app/routes/services.js
+++ app/routes/services.js
@@ -1,5 +1,5 @@
+import ENV from 'suitepad/config/environment';
 import Route from '@ember/routing/route';
-const url = 'https://gist.githubusercontent.com/Rio517/c523873cd4495456a88cac8f1860461b/raw/81667cb58db57cae093092748225c3a98a43ee1e/checks.json';
 
 export default class Application extends Route {
   async _getData(url) {
@@ -11,6 +11,6 @@ export default class Application extends Route {
   }
 
   model() {
-    return this._getData(url);
+    return this._getData(ENV.APP.API_ENDPOINT);
   }
 }
